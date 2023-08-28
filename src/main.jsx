3277981e10cc053b7d26849a03ef24e8c2939ecd
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-08-27 21:28:26 -0300

    criando arquivo global de estilo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { GlobalStyle } from './main.style'
 import { AppRoutes } from './routes/AppRoutes'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle/>
     <AppRoutes />
   </React.StrictMode>,
 )
