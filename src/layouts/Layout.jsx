5568424791aa89179f3a47f6e76bce1215a5f7d8
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-08-30 16:03:13 -0300

    remoção imput pagina erro

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Outlet, useNavigate, useLocation} from 'react-router-dom'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
 import { HeaderComponent } from '../components/header/HeaderComponent'
 import ButtonComponent from '../components/button/ButtonComponent'
 import * as Styled from "./AppLayout.style";
@@ -13,19 +13,21 @@ export const Layout = () => {
         const route = location.pathname.includes('/todo') ? '/' : '/todo';
         navigate(route);
     }
-  return (
-    <Styled.Layout>
-        <HeaderComponent/>
-        <Styled.Main>
-            <Styled.ActionContainer>
-               <ButtonComponent onClick={handleClick}>
-                { location.pathname.includes('/todo') ? 'Ver Tarefas' : 'Nova Tarefa'}
-               </ButtonComponent>
-            </Styled.ActionContainer>
-            <Styled.MainContrainer>
-                <Outlet/>
-            </Styled.MainContrainer>
-        </Styled.Main>
-    </Styled.Layout>
-  )
+    return (
+        <Styled.Layout>
+            <HeaderComponent />
+            <Styled.Main>
+                {location.pathname === '/' || location.pathname.includes('/todo') &&
+                    <Styled.ActionContainer>
+                        <ButtonComponent onClick={handleClick}>
+                            {location.pathname.includes('/todo') ? 'Ver Tarefas' : 'Nova Tarefa'}
+                        </ButtonComponent>
+                    </Styled.ActionContainer>
+                }
+                <Styled.MainContrainer>
+                    <Outlet />
+                </Styled.MainContrainer>
+            </Styled.Main>
+        </Styled.Layout>
+    )
 }
