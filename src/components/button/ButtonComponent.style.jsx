2166bb38c7ff041a2d513ab9f6030a60627ef072
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-08-30 16:36:48 -0300

    implemantando efeito hover e pointer no button

--- src/components/button/ButtonComponent.style.jsx
+++ src/components/button/ButtonComponent.style.jsx
@@ -1,23 +1,34 @@
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import {StyleUtils} from '../../utils/style'
+import { StyleUtils } from '../../utils/style'
 
 const colors = StyleUtils.Colors;
 
 export const Button = styled.button`
     display: flex;
-    padding: ${({$simetric})=> $simetric ? '8px': "8px 24px"};
+    padding: ${({ $simetric }) => $simetric ? '8px' : "8px 24px"};
     justify-content: center;
     align-items: center;
     gap: 10px;
     border: 0;
     border-radius: 5px;
-    background: ${({$bg}) => !$bg ? colors.primary : $bg};
-    color: ${({$color}) => !$color ? '#ffffff' : $color};
+    background: ${({ $bg }) => !$bg ? colors.primary : $bg};
+    color: ${({ $color }) => !$color ? '#ffffff' : $color};
     font-size: 14px;
     font-style: normal;
     font-weight: 400;
     line-height: normal;
+    cursor: pointer;
+    opacity:  .8;
+    transition: all .5s;
+
+    &:hover{
+        opacity: 1;
+    }
+    &:disabled{
+        opacity: .5;
+        cursor: not-allowed;
+    }
 `
 Button.propTypes = {
     $color: PropTypes.string,
