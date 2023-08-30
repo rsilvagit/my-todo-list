20ee38373cfb026c510cc39fad8c70c134c78ada
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-08-30 16:25:15 -0300

    estilização error page

--- src/pages/error/ErrorPage.Style.jsx
+++ src/pages/error/ErrorPage.Style.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
+import { StyleUtils } from '../../utils/style';
 
+const colors = StyleUtils.Colors
 export const ErrorContainer = styled.div`
     display: flex;
     padding: 24px;
@@ -9,7 +11,20 @@ export const ErrorContainer = styled.div`
     gap: 16px;
     flex: 1 0 0;
     align-self: stretch;
+    color: ${colors.primary};
+    font-size: normal;
+    line-height: normal;
     border-radius: 5px;
     background: #FFF;
     box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+    h3 {
+        font-size: 24px;
+        font-weight: 700;
+    }
+    p {
+        font-size: 16px;
+        font-weight: 400;
+
+    }
 `;
